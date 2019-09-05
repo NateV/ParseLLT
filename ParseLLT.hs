e95583c1463ac26b10c3c67ee9703b297c204e51
Nate Vogel <nathan.c.vogel@gmail.com>
2019-09-05 12:51:38 -0400

    added extractDates function to get Dates out of the text

--- ParseLLT.hs
+++ ParseLLT.hs
@@ -6,12 +6,12 @@ import Data.Bits ((.|.))
 import Data.Array
 import Text.Read (readMaybe)
 
-example = "P agrees to pay $60 on 2019-01-01. P will vacate the premises on 2019-02-02. P will then go on vacation."
+example = "P agrees to pay $60 on 02/02/2000. P will vacate the premises on 01/01/2000. P will then go on vacation."
 
 type Line = String
 type Money = Maybe Integer 
 data Token = GenericToken Line
-   | VacateToken [Day] Line
+   | VacateToken [Maybe Day] Line
    | PaymentToken [Money] Line
 
 instance Show Token where 
@@ -24,17 +24,19 @@ splitLines t = fmap unpack $ splitOn (pack ".") (pack t)
 tokenize :: Line -> Token
 tokenize l
     | l =~ "pay" = PaymentToken (extractPayments l)  l
-    | l =~ "vaca" = VacateToken [fromGregorian 2019 1 1] l
+    | l =~ "vaca" = VacateToken (extractDates l) l
     | otherwise = GenericToken l
-
-main = do
-    putStrLn $ show $ fmap tokenize (splitLines example)
 
 --regex options
 compilationOptions = defaultCompOpt .|. compCaseless
 
 caselessMoneyRegex :: Regex
 caselessMoneyRegex = makeRegexOpts compilationOptions defaultExecOpt "\\$[0-9]+(\\.[0-9]+)?"
+
+
+caselessDayRegex :: Regex
+caselessDayRegex = makeRegexOpts compilationOptions defaultExecOpt "\\d{2}/\\d{2}/\\d{2,4}"
+
 
 extractPayments :: Line -> [Money]
 extractPayments l = fmap (readMaybe . removeDollarSign) (extractMatches matches) :: [Money]
@@ -43,3 +45,13 @@ extractPayments l = fmap (readMaybe . removeDollarSign) (extractMatches matches)
 removeDollarSign s = [ c | c <- s, not (c `elem` "$")]
 
 extractMatches matches = fmap (fst . Prelude.head . elems) matches 
+
+extractDates :: Line -> [Maybe Day]
+extractDates l = fmap (parseTimeM True defaultTimeLocale "%m/%d/%Y") (extractMatches matches) :: [Maybe Day]
+    where matches = matchAllText caselessDayRegex l
+
+
+main = do
+    putStrLn $ show $ fmap tokenize (splitLines example)
+
+
