85902628958406cb6246630353583964c51186f6
Nate Vogel <nathan.c.vogel@gmail.com>
2019-09-06 12:01:58 -0400

    got IO working. Man, IO in haskell is confusing!

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,38 @@
+module Main where
+
+
 import Data.Time
 import Text.Regex.PCRE
 import Data.Text
 import Data.Bits ((.|.))
 import Data.Array
 import Text.Read (readMaybe)
-import Control.Applicative 
+import Control.Applicative
 import Data.Csv
 import GHC.Generics (Generic)
 import qualified Data.ByteString.Lazy as BL
 import Data.ByteString.UTF8 as BLU
 import qualified Data.Vector as V
+import qualified Data.Foldable as Foldable
 
 example = "P agrees to pay $60 on 02/02/2000. P will vacate the premises on 01/01/2000. P will then go on vacation."
 
 type Line = String
-type Money = Maybe Integer 
+type Money = Maybe Integer
 data Token = GenericToken Line
    | VacateToken [Maybe Day] Line
    | PaymentToken [Money] Line
 
-instance Show Token where 
+instance Show Token where
     show (GenericToken l) = "Generic:" ++ show l
     show (VacateToken ds l) = "Vacate:" ++ show ds
     show (PaymentToken ms l) = "Payment:" ++ show ms
 
 type TokenAST = [Token]
+
+extractMoney m = case m of
+    Nothing -> 0
+    Just amount -> amount
 
 splitLines :: String -> [Line]
 splitLines t = fmap unpack $ splitOn (pack ".") (pack t)
@@ -51,7 +59,7 @@ extractPayments l = fmap (readMaybe . removeDollarSign) (extractMatches matches)
 
 removeDollarSign s = [ c | c <- s, not (c `elem` "$")]
 
-extractMatches matches = fmap (fst . Prelude.head . elems) matches 
+extractMatches matches = fmap (fst . Prelude.head . elems) matches
 
 extractDates :: Line -> [Maybe Day]
 extractDates l = fmap (parseTimeM True defaultTimeLocale "%m/%d/%Y") (extractMatches matches) :: [Maybe Day]
@@ -64,23 +72,59 @@ sumPayments ps = Prelude.foldr (liftA2 (+)) (Just 0) ps
 data JBARow = JBARow {
                         id :: !String
                       , jba_substance_additional :: !String
-                     }
+                     } deriving (Show)
+
+data JBARowOut = JBARowOut {
+                          outputId :: !String
+                        , money :: !Integer
+                        , full_text :: !String
+                        } deriving (Show)
 
 
 
 instance FromNamedRecord JBARow where
     parseNamedRecord r = JBARow <$> r .: (BLU.fromString "id") <*> r .: (BLU.fromString "jba_substance_additional")
 
+instance ToNamedRecord JBARowOut where
+    toNamedRecord (JBARowOut i m j) = namedRecord [
+        BLU.fromString "outputId" .= i,
+        BLU.fromString "money" .= m,
+        BLU.fromString "full_text" .= j]
+
+instance DefaultOrdered JBARowOut where
+    headerOrder _ = header [
+        BLU.fromString "outputId",
+        BLU.fromString "money",
+        BLU.fromString "full_text"]
+
+parseData :: BL.ByteString -> Either String (V.Vector JBARowOut)
+parseData csvData =
+    case decodeByName csvData of
+        Left err -> Left err
+        Right (h, v) -> V.forM v $ \p -> Right (
+            JBARowOut
+                (Main.id p)
+                ((extractMoney . sumPayments . extractPayments . jba_substance_additional) p)
+                (jba_substance_additional p)
+            )
+
+saveCSV :: Either String (V.Vector JBARowOut) -> IO ()
+saveCSV parsedData =
+    case parsedData of
+        Left err -> putStrLn err
+        Right (v) -> BL.writeFile "out.csv" $ encodeDefaultOrderedByName $ Foldable.toList v
+
+        -- Right (v) -> V.forM_ v $ \p ->
+        --     BL.appendFile "out.csv"
+        --        (encodeDefaultOrderedByName [p])
+
+
+--          ( ((sumPayments . extractPayments . jba_substance_additional) p),
+--                   ((extractDates . jba_substance_additional) p) ))
+
 main = do
     putStrLn $ show $ fmap tokenize (splitLines example)
     putStrLn "Now opening csv"
-    csvData <- BL.readFile "/home/nvogel/Downloads/jbaAdditional.csv"
-    newDate <- case decodeByName csvData of
-        Left err -> IO err
-        Right (_, v) -> IO $ V.forM v $ \ p ->
-          toRecord ((sumPayments . extractPayments . jba_substance_additional) p)
-                   ((show . extractDates . jba_substance_additional) p)
-    putStrLn "Done" 
-    
-
-
+    csvData <- BL.readFile "/home/nate/Downloads/jbaAdditional.csv"
+    saveCSV $ parseData csvData
+    putStrLn "Done"
